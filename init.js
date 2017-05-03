3d4d6353f6dce90c19cb041fe1e4f3ad83603d80
bachtiaryayan <yayan.bachtiar@ralali.com>
2017-05-03 13:26:18 +0700

    add notif mail on succes backup

--- init.js
+++ init.js
@@ -3,6 +3,17 @@ const MysqlTools = require('mysql-tools');
 let schedule = require('node-schedule');
 const tool = new MysqlTools();
 
+const nodemailer = require('nodemailer');
+
+// create reusable transporter object using the default SMTP transport
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'yayan.bachtiar@ralali.com',
+        pass: 'password'
+    }
+});
+
 let j = schedule.scheduleJob('59 59 23 * * *', function () {
 
     tool.dumpDatabase({
@@ -26,6 +37,22 @@ let j = schedule.scheduleJob('59 59 23 * * *', function () {
             } else {
                 console.log(output);
                 console.log(message);
+                // setup email data with unicode symbols
+                let mailOptions = {
+                    from: '"yayan" <yayan.bachtiar@ralali.com>', // sender address
+                    to: 'abiedoank@gmail.com, yayan.untuk.fb@gmail.com, engineer.diy@ralali.com', // list of receivers
+                    subject: '✔ Success backup database ', // Subject line
+                    text: '✔ Hello world ?'+message, // plain text body
+                    html: '<b>Backup data selesai  ?</b> '+ message // html body
+                };
+
+                // send mail with defined transport object
+                transporter.sendMail(mailOptions, (error, info) => {
+                    if (error) {
+                        return console.log(error);
+                    }
+                    console.log('Message %s sent: %s', info.messageId, info.response);
+                });
             }
         });
     })
