23ee60090fd1d85d4c5a9600fa0540153ccb791f
bachtiaryayan <yayan.bachtiar@ralali.com>
2017-05-03 11:53:25 +0700

    init

---
+++ init.js
@@ -0,0 +1,30 @@
+'use strict'
+const mysqlDump = require('mysqldump');
+const MysqlTools = require('mysql-tools');
+
+const tool = new MysqlTools();
+
+tool.dumpDatabase({
+    host: 'localhost',
+    user: 'root',
+    password: 'a',
+    dumpPath: './',
+    database: 'remotedb'
+}, function (error, output, message, dumpFileName) {
+    console.log(error, output, message, dumpFileName)
+    let db = './' + dumpFileName
+    tool.restoreDatabase({
+        host: 'localhost',
+        user: 'root',
+        password: 'a',
+        sqlFilePath: db,
+        database: 'destination-db'
+    }, function (error, output, message) {
+        if (error instanceof Error) {
+            console.log(error);
+        } else {
+            console.log(output);
+            console.log(message);
+        }
+    });
+})
