6a689ea691ff9417cb784f1a7d8fb332e9c14b61
bachtiaryayan <yayan.bachtiar@ralali.com>
2017-05-03 12:53:09 +0700

    add nodejs scheduler

--- init.js
+++ init.js
@@ -1,29 +1,32 @@
 'use strict'
 const MysqlTools = require('mysql-tools');
-
+let schedule = require('node-schedule');
 const tool = new MysqlTools();
 
-tool.dumpDatabase({
-    host: 'localhost',
-    user: 'root',
-    password: 'a',
-    dumpPath: './',
-    database: 'remotedb'
-}, function (error, output, message, dumpFileName) {
-    console.log(error, output, message, dumpFileName)
-    let db = './' + dumpFileName
-    tool.restoreDatabase({
+let j = schedule.scheduleJob('59 59 23 * * *', function () {
+
+    tool.dumpDatabase({
         host: 'localhost',
         user: 'root',
         password: 'a',
-        sqlFilePath: db,
-        database: 'destination-db'
-    }, function (error, output, message) {
-        if (error instanceof Error) {
-            console.log(error);
-        } else {
-            console.log(output);
-            console.log(message);
-        }
-    });
-})
+        dumpPath: './',
+        database: 'test-gembus'
+    }, function (error, output, message, dumpFileName) {
+        console.log(error, output, message, dumpFileName)
+        let db = './' + dumpFileName
+        tool.restoreDatabase({
+            host: 'localhost',
+            user: 'root',
+            password: 'a',
+            sqlFilePath: db,
+            database: 'test-backup'
+        }, function (error, output, message) {
+            if (error instanceof Error) {
+                console.log(error);
+            } else {
+                console.log(output);
+                console.log(message);
+            }
+        });
+    })
+});
